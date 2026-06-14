d99b93c03e4deb47806ed7ede1bec4b1dcd558c4
Erwin <erwin.rakoto@gmail.com>
2026-06-14 13:28:53 +0200

    feat: init FastAPI structure code

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
+from passenger.api import router as passenger_router
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(passenger_router, prefix="/passenger")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
